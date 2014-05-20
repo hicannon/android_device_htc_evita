56e26ea096db719ebae76c6cb49b292fcc6f2867
JoseGalRe <josegalre@gmail.com>
2014-05-20 16:25:17 -0600

    evita: remove unused input device configs These are already included in frameworks/base/data/keyboards. This cleans up a little noise when running lunch.
    See Here: https://github.com/CyanogenMod/android_device_htc_evita/commit/6fc14c86cec81aa7693caebc9da54d802fc88332

--- evita.mk
+++ evita.mk
@@ -281,8 +281,6 @@ PRODUCT_COPY_FILES += \
 # Input Device Config
 PRODUCT_COPY_FILES += \
     device/htc/evita/prebuilt/usr/idc/projector_input.idc:system/usr/idc/projector_input.idc \
-    device/htc/evita/prebuilt/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
-    device/htc/evita/prebuilt/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
     device/htc/evita/prebuilt/usr/idc/synaptics-rmi-touchscreen.idc:system/usr/idc/synaptics-rmi-touchscreen.idc
 
 # Device Default Properties
